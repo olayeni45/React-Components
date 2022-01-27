c10c8174a98ee09771bf733bcc12fa78031b891d
Olayeni Anifowose <olayeni45@gmail.com>
2022-01-27 12:03:15 +0000

    css modules

--- components/src/CourseGoals/CourseInput/CourseInput.jsx
+++ components/src/CourseGoals/CourseInput/CourseInput.jsx
@@ -1,35 +1,6 @@
 import React, { useState } from "react";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 import Button from "../../UI/Button";
-import styled from "styled-components";
-
-const FormControl = styled.div`
-  margin: 0.5rem 0;
-
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${({ invalid }) => (invalid ? "red" : "black")}
-  }
-
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${({ invalid }) => (invalid ? "red" : "#ccc")};
-    background-color: ${({ invalid }) => (invalid ? "#ffd7d7" : "transparent")}
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-
-`;
 
 const CourseInput = (props) => {
   const { onAddGoal } = props;
@@ -56,13 +27,45 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div
+        className={`${styles["form-control"]}  ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={inputHandler} value={input} />
-      </FormControl>
+      </div>
       <Button type="submit"> Add Goal</Button>
     </form>
   );
 };
 
 export default CourseInput;
+
+// import styled from "styled-components";
+
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${({ invalid }) => (invalid ? "red" : "black")}
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${({ invalid }) => (invalid ? "red" : "#ccc")};
+//     background-color: ${({ invalid }) => (invalid ? "#ffd7d7" : "transparent")}
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+
+// `;
